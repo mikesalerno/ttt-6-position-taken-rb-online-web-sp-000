d4e7a95c7a5c54506e17482160d162d5a0cd827c
Michael <wtfdic@hotmail.com>
2020-11-09 20:32:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,6 @@
 def position_taken?(board, index)
   if board[index] == " "
     return false
-  elsif board[index] == ""
-    return false
-  elsif board[index]  == nil
-    return false
   else
     return true
   end
